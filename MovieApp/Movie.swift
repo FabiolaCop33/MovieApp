bf5e6eb4214ab5cd2aa78fea019008f8a166685c
Fabiola Correa Padilla <129307826+FabiolaCop33@users.noreply.github.com>
2024-10-22 18:41:49 -0600

    Firs Commit

---
+++ MovieApp/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by Familia Alvarado Perez on 22/10/24.
+//
+
+import Foundation
+
---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Familia Alvarado Perez on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
